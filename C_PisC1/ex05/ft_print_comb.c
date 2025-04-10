326ed9dedcbca5bb6a1a7c54988cb334b3d01af7
Andrew Ong <anong@u82z04s06.42kl.edu.my>
2025-04-10 17:12:07 +0800

    push regularly

--- C_PisC1/ex05/ft_print_comb.c
+++ C_PisC1/ex05/ft_print_comb.c
@@ -6,41 +6,31 @@
 /*   By: anong <anong@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 14:17:45 by anong             #+#    #+#             */
-/*   Updated: 2025/04/10 14:41:09 by anong            ###   ########.fr       */
+/*   Updated: 2025/04/10 17:09:03 by anong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_print_comb(void);
+void	ft_write(int i, int j, int k);
+
 void	ft_print_comb(void)
 {
-	int i;
-	int j;
-	int k;
-	char a;
-	char b;
-	char c;
+	int	i;
+	int	j;
+	int	k;
 
 	i = 0;
 	j = 1;
 	k = 2;
-
 	while (i < 8)
 	{
 		while (j < 9)
 		{
 			while (k < 10)
 			{
-				if (i < j && j < k)
-				{
-					a = i + '0';
-					b = j + '0';
-					c = k + '0';
-					write(1, &a, 1);
-					write(1, &b, 1);
-					write(1, &c, 1);
-					write(1, ", ", 2);
-				}
+				ft_write(i, j, k);
 				k = k + 1;
 			}
 			j = j + 1;
@@ -49,6 +39,21 @@ void	ft_print_comb(void)
 		i = i + 1;
 		j = i;
 	}
+}
+
+void	ft_write(int i, int j, int k)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = i + '0';
+	b = j + '0';
+	c = k + '0';
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	write(1, ", ", 2);
 }
 
 int	main(void)
